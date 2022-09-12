f7c5a79165da4add9ebb5d556c7265e27077ead6
Fredrik Nestvold Larsen <fredrik@pc45-198.eduroam.uib.no>
2022-09-12 08:48:45 +0200

    Accordion now work with person

--- frontend/src/component/Accordion.tsx
+++ frontend/src/component/Accordion.tsx
@@ -3,75 +3,106 @@ import styled from 'styled-components'
 import { FcOk, FcHighPriority } from "react-icons/fc";
 import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 import { CMS } from '../assets/db.json'
+import { SetStateAction, useState } from 'react';
 
-const Accordion = ({ className }: any) => {
+const Accordion = ({ className, data }: any) => {
 
-  const data = {
-    aliases: "Hareide, Knut Arild",
-    birth_date: "1972-11-23",
-    countries: "no",
-    dataset: "Every Politician",
-    emails: "",
-    first_seen: "2021-10-07 03:02:59",
-    id: "evpo-ff10bc5a-4aad-4895-b886-fb88e598ed57",
-    identifiers: "Q1350399",
-    last_seen: "2021-07-26 11:55:45",
-    name: "Knut Arild Hareide",
-    phones: "",
-    sanctions: "",
-    schema: "Person",
-    score: 0.0003451437393358565
+  const [accordion, setActiveAccordion] = useState(-1)
+
+  function toggleAccordion(index: SetStateAction<number>) {
+    if (index === accordion) {
+      setActiveAccordion(-1)
+      return
+    }
+    setActiveAccordion(index);
+  }
+
+  function BoldText({ children }: any) {
+    return <span style={{ fontWeight: 'bold' }}>{children}</span>;
   }
 
   return (
     <div className={className}>
-      <button className={className}>
-        <div className='container'>
-          <FcOk size={30} />
-          <div className='header'>
-            <p className='text'>{CMS.ACCORDION_TARGET_NAME} </p>
-            <p className='text'>{CMS.ACCORDION_TARGET_ID} </p>
+      <h2>{CMS.RESULT_HEADER}</h2>
+      <div className='results'>
+        {data.map((item: any, index: number) =>
+          < div className='container' key={index} onClick={() => toggleAccordion(index)}>
+            <div className='header'>
+              <div className='summary'>
+                <p className='text'>{CMS.ACCORDION_TARGET_NAME} {item.name}</p>
+                <p className='text'>{CMS.ACCORDION_TARGET_ID} {item.identifiers}</p>
+              </div>
+              {accordion === index ? (
+                <span>
+                  <BsChevronUp size={40} />
+                </span>
+              ) : (
+                <span>
+                  <BsChevronDown size={40} />
+                </span>
+              )}
+            </div>
+            <div className={accordion === index ? 'active' : 'inactive'}>
+              <ul>
+                {Object.entries(item).map(([key, value]) => (
+                  <li key={key}>
+                    <>
+                      <BoldText>{key}</BoldText> : {value}
+                    </>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <BsChevronDown size={40} />
-        </div>
-      </button>
-      <div className='details'>
-        {data.aliases}
+        )}
       </div>
-    </div>
+    </div >
   )
 }
 
 export default styled(Accordion)`
+  padding-top: 100px;
+
+  .container {
+    text-align: start;
+    padding: 15px;
+  }
+
+  .summary {
     display: flex;
     flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
-    justify-content: center;
+    width: 650px;
+  }
 
-    width: 700px;
-    height: 108px;
-    background-color: rgb(243, 241, 240);
-    color: rgb(45, 44, 44);
+  .text {
+    font-size: 16px;
+    font-weight: bold;
+  }
 
-    .header {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
+  .accordion-simple > .active {
+    display: block;
+    align-content: flex-start;
+  }
 
-    }
-    .container {
-      width: 600px;
-      height: inherit;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      align-content: center;
-    }
+  .results .inactive {
+    display: none;
+  }
 
-    .text {
-      font-size: 16px;
-      font-weight: bold;
-    }
-
+  .results > div {
+    color: rgb(45, 45, 45);
+    background-color: rgb(245, 240, 240);
+    margin-bottom: 20px;
+    border-radius: 2px;
+    cursor: pointer;
+    width: 670px;
+    height: inherit;
+  }
 `
